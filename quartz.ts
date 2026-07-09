72125a576c3e56b3d4ef86d98256e596196274b9
Petr Liakhavets <wenaught@gmail.com>
2026-07-09 20:44:02 +0300

    Quartz sync: Jul 9, 2026, 8:44 PM

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,18 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { FileTrieNode } from "./quartz/util/fileTrie"
+import * as ExternalPlugin from "./.quartz/plugins"
+
+ExternalPlugin.Explorer({
+  filterFn: (node: FileTrieNode) => {
+    // set containing names of everything you want to filter out
+    const omit = new Set(["заклинания"])
+ 
+    // can also use node.slug or by anything on node.data
+    // note that node.data is only present for files that exist on disk
+    // (e.g. implicit folder nodes that have no associated index.md)
+    return !omit.has(node.displayName.toLowerCase())
+  },
+})
 
 const config = await loadQuartzConfig()
 export default config
