6225800519262e03ab93b97f5e96bb03adede55d
Petr Liakhavets <wenaught@gmail.com>
2026-07-10 12:11:27 +0300

    Quartz sync: Jul 10, 2026, 12:11 PM

--- quartz.ts
+++ quartz.ts
@@ -4,13 +4,8 @@ import * as ExternalPlugin from "./.quartz/plugins"
 
 ExternalPlugin.Explorer({
   filterFn: (node: FileTrieNode) => {
-    // set containing names of everything you want to filter out
-    const omit = new Set(["заклинания"])
- 
-    // can also use node.slug or by anything on node.data
-    // note that node.data is only present for files that exist on disk
-    // (e.g. implicit folder nodes that have no associated index.md)
-    return !omit.has(node.displayName.toLowerCase())
+    const filteredNodes = node.displayName.toLowerCase() == "заклинания" ? [node, ...node.children] : []
+    return !filteredNodes.includes(node)
   },
 })
 
